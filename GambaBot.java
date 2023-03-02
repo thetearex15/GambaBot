6560cef2cd47e39eb0f7254e9ec043c61224ce31
Brian Vo <93850210+thetearex15@users.noreply.github.com>
2023-03-02 17:51:16 -0500

    Add files via upload

---
+++ GambaBot.java
@@ -0,0 +1,55 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+
+import java.util.EnumSet;
+import java.util.concurrent.TimeUnit;
+
+import static net.dv8tion.jda.api.interactions.commands.OptionType.*;
+
+
+public class GambaBot extends ListenerAdapter {
+    public static void main(String[] args) {
+        JDA jda = JDABuilder.createDefault("MTA4MDc3MTY1MDU2OTY1NDI4Mg.G9c5FG.Vi-whU2FIdcHVjOPmSClOvztn-_HjNfM_b84qQ")
+                .addEventListeners(new GambaBot())
+                .build();
+
+        CommandListUpdateAction commands = jda.updateCommands();
+
+        commands.addCommands(
+                Commands.slash("coins", "Shows amount of coins."),
+                Commands.slash("coinflip", "Do a coin flip.")
+                        .addOptions(new OptionData(USER, "user", "The user to challenge.")),
+                Commands.slash( "roulette", "Spins the wheel."),
+                Commands.slash( "shop", "Opens up the shop where you can spend your coins.")
+        );
+        commands.queue();
+
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        switch (event.getName()){
+            case "coins":
+                Member member = event.getMember();
+            case "coinflip":
+                event.reply("No.").queue();
+        }
+    }
+
+
+}
