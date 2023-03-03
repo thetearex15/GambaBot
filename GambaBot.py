a22cc012365d2757836058f10ec5d3cdbd7692d2
Barrett Williamson <williamsonbarrett@gmail.com>
2023-03-03 02:14:55 +0000

    Clean up database entrys. Only init user once

--- GambaBot.py
+++ GambaBot.py
@@ -13,6 +13,13 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS money
              (user_id TEXT PRIMARY KEY, balance INTEGER, last_daily INTEGER, cap INTEGER)''')
 
+# Init the user if it is their first message
+def init_user(user_id):
+        balance = 0
+        last_daily = 0
+        cap = 10000
+        c.execute('REPLACE INTO money (user_id, balance, last_daily, cap) VALUES (?, ?, ?, ?)', (user_id, balance, 0, cap))
+
 # The coinflip command
 @bot.command()
 async def flip(ctx, amount: int):
@@ -21,10 +28,9 @@ async def flip(ctx, amount: int):
     result = c.fetchone()
 
     if result is None:
-        balance = 0
-        cap = 10000
-    else:
-        balance, cap = result
+        await ctx.send("You need to use the daily command to start playing.")
+        return 
+    balance, cap = result
 
     if balance + amount > cap:
         await ctx.send("You have reached your money cap. Use `!upgrade_cap` command to increase your cap.")
@@ -38,7 +44,7 @@ async def flip(ctx, amount: int):
         new_balance = balance - amount
         await ctx.send(f"Sorry, you lost {amount} coins. Your new balance is {new_balance} coins.")
 
-    c.execute('REPLACE INTO money (user_id, balance, last_daily, cap) VALUES (?, ?, ?, ?)', (user_id, new_balance, int(time.time()), cap))
+    c.execute('UPDATE money SET balance = ? WHERE user_id = ?', (new_balance, user_id))
     conn.commit()
 
 # Daily reward command
@@ -47,22 +53,25 @@ async def daily(ctx):
     user_id = str(ctx.author.id)
     c.execute('SELECT balance, last_daily, cap FROM money WHERE user_id=?', (user_id,))
     result = c.fetchone()
-
+    amount = 100
     if result is None:
-        balance = 0
-        last_daily = 0
-        cap = 10000
-    else:
-        balance, last_daily, cap = result
+        init_user(user_id)
+        c.execute('SELECT balance, last_daily, cap FROM money WHERE user_id=?', (user_id,))
+        result = c.fetchone()
+    balance, last_daily, cap = result
 
     if time.time() - last_daily < 86400:
         await ctx.send("You can only claim your daily reward once every 24 hours.")
         return
+    
+    if balance + amount > cap:
+        await ctx.send("You have reached your money cap. Use `!upgrade_cap` command to increase your cap.")
+        return
+    
+    new_balance = balance + amount
+    await ctx.send(f"You claimed your daily reward of 100 coins! Your new balance is {new_balance} coins.")
 
-    new_balance = balance + 10
-    await ctx.send(f"You claimed your daily reward of 10 coins! Your new balance is {new_balance} coins.")
-
-    c.execute('REPLACE INTO money (user_id, balance, last_daily, cap) VALUES (?, ?, ?, ?)', (user_id, new_balance, int(time.time()), cap))
+    c.execute('UPDATE money SET balance = ?, last_daily = ? WHERE user_id = ?', (new_balance, int(time.time()), user_id))
     conn.commit()
 
 @bot.command()
@@ -72,7 +81,7 @@ async def upgrade_cap(ctx):
     result = c.fetchone()
 
     if result is None:
-        await ctx.send("You need to start playing the game to increase your cap.")
+        await ctx.send("You need to use the daily command to start playing.")
         return
 
     cap, balance = result
@@ -85,7 +94,7 @@ async def upgrade_cap(ctx):
     new_balance = balance - int(cap * 0.8)
     await ctx.send(f"You upgraded your money cap to {new_cap} coins by paying {int(cap * 0.8)} coins.")
 
-    c.execute('REPLACE INTO money (user_id, balance, cap) VALUES (?, ?, ?)', (user_id, new_balance, new_cap))
+    c.execute('UPDATE money SET balance = ?, cap = ? WHERE user_id = ?', (new_balance, new_cap, user_id))
     conn.commit()
 
 bot.run(os.getenv('DISCORD_TOKEN'))
